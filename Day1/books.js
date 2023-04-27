f2493c82fbec7a81d54cc00dfac620ccfae14512
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-04-27 15:15:05 +0200

    lab 1

---
+++ Day1/books.js
@@ -0,0 +1,138 @@
+books = [
+    {
+      "name":'lord ',
+      "image": `https://m.media-amazon.com/images/I/71jLBXtWJWL._AC_UF1000,1000_QL80_.jpg`,
+      "category": "Fiction",
+      "author": "Jane Doe",
+      "pages": 350,
+      "price": 19.99,
+      "isbn": "978-1-2345-6789-0"
+    },
+    {
+      "name":'Harry Potter part 2 ',
+      "image": `https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg`,
+      "category": "Non-Fiction",
+      "author": "John Smith",
+      "pages": 60,
+      "price": 14.99,
+      "isbn": "978-0-9876-5432-1"
+    },
+    {
+      "name":'Harry Potter part 1 ',
+      "image": `https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/31/10/HP1.jpg`,
+      "category": "Mystery",
+      "author": "Sue Jones",
+      "pages": 420,
+      "price": 24.99,
+      "isbn": "978-3-2100-0000-1"
+    },
+    {
+      "name":'Dave Oliver ',
+      "image": `https://thenobleartist.com/wp-content/uploads/2018/09/trial3_4.jpg`,
+      "category": "Biography",
+      "author": "Tom Brown",
+      "pages": 560,
+      "price": 29.99,
+      "isbn": "978-6-7890-1234-5"
+    },
+    {
+      "name":'The Emporeres Knife ',
+      "image": `https://i.pinimg.com/736x/29/66/13/29661304646ad0c486813826f0918144.jpg`,
+      "category": "Science Fiction",
+      "author": "Lisa Green",
+      "pages": 40,
+      "price": 17.99,
+      "isbn": "978-9-8765-4321-0"
+    },
+    {
+      "name":'The Emporeres Knife ',
+      "image": `https://thenobleartist.com/wp-content/uploads/2015/10/ChildOfTheKindred_ebook1.jpg`,
+      "category": "Science Fiction",
+      "author": "Lisa Green",
+      "pages": 40,
+      "price": 17.99,
+      "isbn": "978-9-8765-4321-0"
+    },
+    {
+      "name":'Dave Oliver ',
+      "image": `https://www.bookcoversart.com/wp-content/uploads/2020/01/Fantasy-Book-Cover-The-Exiled_1.jpg`,
+      "category": "Biography",
+      "author": "Tom Brown",
+      "pages": 560,
+      "price": 29.99,
+      "isbn": "978-6-7890-1234-5"
+    },
+    {
+      "name":'The Emporeres Knife ',
+      "image": `https://img.buzzfeed.com/buzzfeed-static/static/2015-12/8/15/enhanced/webdr12/original-grid-image-18653-1449606164-4.jpg?crop=625:938;0,1`,
+      "category": "Science Fiction",
+      "author": "Lisa Green",
+      "pages": 40,
+      "price": 11.22,
+      "isbn": "978-9-8765-4321-0"
+    },
+    {
+      "name":'The Emporeres Knife ',
+      "image": `https://i.pinimg.com/736x/29/66/13/29661304646ad0c486813826f0918144.jpg`,
+      "category": "Science Fiction",
+      "author": "Lisa Green",
+      "pages": 200,
+      "price": 22.99,
+      "isbn": "978-9-8765-4321-0"
+    },
+    {
+      "name":'lord ',
+      "image": `https://m.media-amazon.com/images/I/71jLBXtWJWL._AC_UF1000,1000_QL80_.jpg`,
+      "category": "Fiction",
+      "author": "Jane Doe",
+      "pages": 350,
+      "price": 19.99,
+      "isbn": "978-1-2345-6789-0"
+    },
+    {
+      "name":'Harry Potter part 2 ',
+      "image": `https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg`,
+      "category": "Non-Fiction",
+      "author": "John Smith",
+      "pages": 60,
+      "price": 14.99,
+      "isbn": "978-0-9876-5432-1"
+    },
+    {
+      "name":'Harry Potter part 1 ',
+      "image": `https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/07/31/10/HP1.jpg`,
+      "category": "Mystery",
+      "author": "Sue Jones",
+      "pages": 420,
+      "price": 24.99,
+      "isbn": "978-3-2100-0000-1"
+    },
+    
+    {
+      "name":'Dave Oliver ',
+      "image": `https://thenobleartist.com/wp-content/uploads/2018/09/trial3_4.jpg`,
+      "category": "Biography",
+      "author": "Tom Brown",
+      "pages": 560,
+      "price": 29.99,
+      "isbn": "978-6-7890-1234-5"
+    },
+    {
+      "name":'The Emporeres Knife ',
+      "image": `https://i.pinimg.com/736x/29/66/13/29661304646ad0c486813826f0918144.jpg`,
+      "category": "Science Fiction",
+      "author": "Lisa Green",
+      "pages": 40,
+      "price": 17.99,
+      "isbn": "978-9-8765-4321-0"
+    },
+    {
+      "name":'The Emporeres Knife ',
+      "image": `https://thenobleartist.com/wp-content/uploads/2015/10/ChildOfTheKindred_ebook1.jpg`,
+      "category": "Science Fiction",
+      "author": "Lisa Green",
+      "pages": 40,
+      "price": 17.99,
+      "isbn": "978-9-8765-4321-0"
+    },
+  ]
